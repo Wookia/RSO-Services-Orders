3a3ff38714bcb1abaf4886317cf81105ebf57ae3
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-05-11 19:45:07 +0200

    Use process.env.* variables for DB connection configuration

--- src/storage/driver.js
+++ src/storage/driver.js
@@ -3,12 +3,11 @@ let Promise = require("bluebird");
 
 
 function createConnection() {
-    // TODO create process.env.* variables in docker configuration
-    const DB_HOST = 'orders-db';
-    const DB_PORT = 5432;
-    const DB_NAME = 'postgres';
-    const DB_USER = 'postgres';
-    const DB_PASSWORD = 'password';
+    const DB_NAME = (process.env.DB_NAME || 'postgres');
+    const DB_USER = (process.env.DB_USER || 'postgres');
+    const DB_PASSWORD = (process.env.DB_PASSWORD || 'password');
+    const DB_HOST = (process.env.DB_HOST || getDbHost());
+    const DB_PORT = (process.env.DB_PORT || 5432);
 
     return new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
         dialect: 'postgres',
@@ -32,6 +31,19 @@ function getValidConnection(dbDriver) {
                 setTimeout(() => resolve(getValidConnection(dbDriver)), 10000);
             });
     });
+}
+
+function getDbHost() {
+    let host = '';
+    const isWin = process.platform === "win32";
+    if (isWin === true) {
+        console.log('System: Windows');
+        host = 'localhost';
+    } else {
+        console.log('System: Linux/Mac');
+        host = '192.168.99.100';
+    }
+    return host;
 }
 
 let sequelize = createConnection();
