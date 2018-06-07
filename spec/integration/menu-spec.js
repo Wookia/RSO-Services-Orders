3892078bb7a5f630860ac4773ee209dadb35d463
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-06-07 03:33:05 +0200

    WIP Add mock for DB

---
+++ spec/integration/menu-spec.js
@@ -0,0 +1,136 @@
+let request = require('request');
+const fs = require('fs');
+
+
+describe('Menu api ', () => {
+    const URL = 'http://localhost:3000';
+    const API = '/api/menu';
+
+    let token;
+
+    function checkErrorAndResponse(error, response) {
+        expect(error).toBe(null);
+        expect(response.statusCode).toBe(200);
+    }
+
+    function checkBody(body, ref) {
+        for (let key in ref) {
+            expect(body[key]).toBeDefined();
+            expect(body[key]).toBe(ref[key]);
+        }
+    }
+
+    const sampleRequestBody = {
+        group: 'GroupName',
+        name: 'DishName',
+        description: 'DishDescription',
+        price: 1234567890
+    };
+
+    beforeAll(() => {
+        fs.unlink('spec/database.sqlite');
+        myService = require("../../src/index.js");
+        myService.startServer();
+
+    }, 60000);
+
+    // describe('GET ', () => {
+    //     it('404 failure on /api/menu with misspell', (done) => {
+    //         request.get(URL + API + '123', function (error, response, body) {
+    //             expect(error).toBe(null);
+    //             expect(response.statusCode).toBe(404);
+    //
+    //             expect(body).toContain("Error");
+    //             expect(body).toContain("Cannot GET /api/menu123");
+    //
+    //             done();
+    //         });
+    //     });
+    //
+    //     it('successful /api/menu', (done) => {
+    //         request.get(URL + API, (error, response, body) => {
+    //             checkErrorAndResponse(error, response);
+    //
+    //             expect(body).toContain("id");
+    //             expect(body).toContain("group");
+    //             expect(body).toContain("name");
+    //             expect(body).toContain("description");
+    //             expect(body).toContain("price");
+    //
+    //             done();
+    //         });
+    //     });
+    // });
+
+    function samplePOST(lambda) {
+        request.post(URL + API,
+            {
+                body: sampleRequestBody,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                },
+                json: true
+            },
+            (error, response, body) => lambda(error, response, body)
+        );
+    }
+
+    describe('POST ', () => {
+        it('successful POST /api/menu', (done) => {
+            samplePOST((error, response, body) => {
+                checkErrorAndResponse(error, response);
+                checkBody(body, sampleRequestBody);
+                done();
+            });
+        });
+    });
+
+    // describe('PUT ', () => {
+    //     it('successful /api/menu/ID', (done) => {
+    //         samplePOST((error, response, body) => {
+    //             checkErrorAndResponse(error, response);
+    //
+    //             let putBody = Object.assign({}, sampleRequestBody);
+    //             for (let key in putBody)
+    //                 putBody[key] += 5;
+    //
+    //             request.put({
+    //                     url: URL + API + '/' + body.id,
+    //                     body: putBody,
+    //                     json: true
+    //                 },
+    //                 (error2, response2, body2) => {
+    //                     checkErrorAndResponse(error2, response2);
+    //                     putBody.id = body.id;
+    //                     checkBody(body2, putBody);
+    //
+    //                     done();
+    //                 });
+    //         });
+    //     });
+    // });
+    //
+    // describe('DELETE ', () => {
+    //     it('successful /api/menu/ID', (done) => {
+    //         samplePOST((error, response, body) => {
+    //             checkErrorAndResponse(error, response);
+    //             checkBody(body, sampleRequestBody);
+    //
+    //             request.delete(URL + API + '/' + body.id, (error2, response2, body2) => {
+    //                 body2 = JSON.parse(body2);
+    //
+    //                 checkErrorAndResponse(error2, response2);
+    //                 checkBody(body2, body);
+    //
+    //                 request.get(URL + API, (error3, response3, body3) => {
+    //                     checkErrorAndResponse(error3, response3);
+    //
+    //                     expect(body3).not.toContain('"id":' + body.id);
+    //                     done();
+    //                 });
+    //             });
+    //         });
+    //     });
+    // });
+});
--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+function start() {
+    const express = require('express');
+    const bodyParser = require('body-parser');
 
-let dbDriverPromise = require('./storage/driver');
-const Menu = require('./api/menu');
-const Orders = require('./api/orders');
+    let dbDriverPromise = require('./storage/driver');
+    const Menu = require('./api/menu');
+    const Orders = require('./api/orders');
 
-let app = express();
+    let app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-dbDriverPromise
-    .then((dbDriver) => {
-        let menu = new Menu(dbDriver);
-        app.use("", menu.getEndPointRouter());
-        let orders = new Orders(dbDriver);
-        app.use("", orders.getEndPointRouter());
-
-        app.listen(process.env.PORT || 3000, () => console.log('Orders backend listening on port: ' + (process.env.PORT || 3000)));
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
     });
+
+    dbDriverPromise
+        .then((dbDriver) => {
+            let menu = new Menu(dbDriver);
+            app.use("", menu.getEndPointRouter());
+            let orders = new Orders(dbDriver);
+            app.use("", orders.getEndPointRouter());
+
+            app.listen(process.env.PORT || 3000, () => console.log('Orders backend listening on port: ' + (process.env.PORT || 3000)));
+        });
+}
+module.exports.startServer = start;
+
+start();
