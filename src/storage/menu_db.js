af8d4c8eadd32e46476ddf96b68328149a32959d
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-06-10 14:19:28 +0200

    Skip adding records if already exist

--- src/storage/menu_db.js
+++ src/storage/menu_db.js
@@ -19,6 +19,14 @@ class MenuDB extends Table {
             };
             this.initialize('menu', modelColumns, true)
                 .then(() => {
+                    return this.getAll();
+                })
+                .then(result => {
+                    if (result.length !== 0) {
+                        resolve();
+                        return;
+                    }
+
                     this.add({group: "Drinks", name: "Wine 1", description: "cheap wine", price: 10})
                         .then(() => {
                             this.add({group: "Drinks", name: "Wine 2", description: "expensive wine", price: 20})
@@ -27,15 +35,12 @@ class MenuDB extends Table {
                             this.add({group: "Drinks", name: "Wine 3", description: "cheap wine", price: 10})
                         })
                         .then(() => {
-                            this.add({group: "MainCourse", name: "Chicken 1", description: "good after gym", price: 10})
+                            this.add({
+                                group: "MainCourse", name: "Chicken 1", description: "good after gym", price: 10})
                         })
                         .then(() => {
                             this.add({
-                                group: "MainCourse",
-                                name: "Chicken 2",
-                                description: "dinner for couple",
-                                price: 20
-                            })
+                                group: "MainCourse", name: "Chicken 2", description: "dinner for couple", price: 20})
                         })
                         .then(() => {
                             resolve();
